758250397de02591bbd9a9960eaafa986e8c0534
Daniel Ferreira <danielblindta@yahoo.com.br>
2024-11-24 19:00:45 -0500

    some changes in class

--- sample_codes/functions.py
+++ sample_codes/functions.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-class Plotter:
-    def __init__(self, data):
+class multiplot:
+    def __init__(self, data, x_columns):
         """
         Initialize the Plotter class with a dataset.
 
@@ -11,8 +11,9 @@ class Plotter:
             data: Input data-frame containing variables to plot.
         """
         self.data = data
+        self.x_columns = x_columns
 
-    def hist_plots(self, columns, rem_ol=False, thres=0.99, scale_graph=9, n_cols=3, aspect_ratio=2/3):
+    def hist_plots(self, rem_ol=False, thres=0.99, scale_graph=9, n_cols=3, aspect_ratio=2/3):
         """
         Create multiple histogram plots for specified columns.
 
@@ -24,13 +25,13 @@ class Plotter:
             n_cols: Number of graphs per row.
             aspect_ratio: Aspect ratio of individual graphs.
         """
-        n_rows = len(columns) // n_cols + (len(columns) % n_cols > 0)
+        n_rows = len(self.x_columns) // n_cols + (len(self.x_columns) % n_cols > 0)
         fig, axes = plt.subplots(n_rows, n_cols, figsize=(scale_graph, (scale_graph / n_cols) * aspect_ratio * n_rows))
 
-        fig.suptitle(f'Histograms of {len(columns)} columns', y=1, size=15)
+        fig.suptitle(f'Histograms of input columns', y=1, size=15)
         axes = axes.flatten()
 
-        for i, feature in enumerate(columns):
+        for i, feature in enumerate(self.x_columns):
             if rem_ol:
                 lim = self.data[feature].quantile([thres]).iloc[0]
                 x = self.data[feature][self.data[feature] < lim]
@@ -38,14 +39,13 @@ class Plotter:
             else:
                 x = self.data[feature]
             sns.histplot(data=x, ax=axes[i], kde=True)
-        
-        # Remove unused subplots
-        for j in range(len(columns), len(axes)):
+
+        for j in range(len(self.x_columns), len(axes)):
             fig.delaxes(axes[j])
 
         plt.tight_layout()
 
-    def violin_plots(self, outcome, x_columns, scale_graph=9, n_cols=3, aspect_ratio=2/3):
+    def violin_plots(self, outcome, scale_graph=9, n_cols=3, aspect_ratio=2/3):
         """
         Create multiple violin plots by a categorical variable.
 
@@ -56,17 +56,15 @@ class Plotter:
             n_cols: Number of graphs per row.
             aspect_ratio: Aspect ratio of individual graphs.
         """
-        n_rows = len(x_columns) // n_cols + (len(x_columns) % n_cols > 0)
+        n_rows = len(self.x_columns) // n_cols + (len(self.x_columns) % n_cols > 0)
         fig, axes = plt.subplots(n_rows, n_cols, figsize=(scale_graph, (scale_graph / n_cols) * aspect_ratio * n_rows))
 
-        fig.suptitle(f'Violin plots of {len(x_columns)} columns by {outcome}', y=1, size=15)
+        fig.suptitle(f'Violin plots of {len(self.x_columns)} columns by {outcome}', y=1, size=15)
         axes = axes.flatten()
 
-        for i, feature in enumerate(x_columns):
+        for i, feature in enumerate(self.x_columns):
             sns.violinplot(data=self.data, ax=axes[i], y=feature, x=outcome)
-
-        # Remove unused subplots
-        for j in range(len(x_columns), len(axes)):
+        for j in range(len(self.x_columns), len(axes)):
             fig.delaxes(axes[j])
 
         plt.tight_layout()
